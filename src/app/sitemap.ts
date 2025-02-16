8fcf27c043f03a9f015a83e295e969bc468a0532
Lucas Winkler <lucaswinkler@gmail.com>
2025-02-16 03:46:29 -0500

    fix: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,14 +2,16 @@ import { config } from "@/config";
 import { PROJECTS } from "@/constants/projects";
 import type { MetadataRoute } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // TODO: Uncomment changeFrequency and priority when the site is live
 export default function sitemap(): MetadataRoute.Sitemap {
-  // const projects = PROJECTS.map(project => ({
-  //   url: `${config.baseUrl}/projects/${project.slug}`,
-  //   lastModified: new Date(),
-  //   // changeFrequency: 'yearly' as const,
-  //   // priority: 0.8,
-  // }));
+  const projects = PROJECTS.map((project) => ({
+    url: `${config.baseUrl}/projects/${project.slug}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly" as const,
+    priority: 0.8,
+  }));
 
   return [
     {
@@ -18,18 +20,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
-    // {
-    // url: `${config.baseUrl}/projects`,
-    // lastModified: new Date(),
-    // changeFrequency: 'monthly',
-    // priority: 0.9,
-    // },
-    // {
-    //   url: `${config.baseUrl}/contact`,
-    //   lastModified: new Date(),
-    //   changeFrequency: 'yearly',
-    //   priority: 0.6,
-    // },
-    // ...projects,
+
+    ...(isProduction
+      ? [
+          {
+            url: `${config.baseUrl}/projects`,
+            lastModified: new Date(),
+            changeFrequency: "monthly" as const,
+            priority: 0.9,
+          },
+          ...projects,
+        ]
+      : []),
   ];
 }
