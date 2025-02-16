79354dcd8196bbc4f2b42e826757492219719f0d
Lucas Winkler <lucaswinkler@gmail.com>
2025-02-15 20:55:51 -0500

    chore: delete about and contact page

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "@/components/layout/container";
-
-export default function About() {
-  return (
-    <Container className="pt-[calc(var(--header-height)/2+4rem)] pb-16 sm:pt-[calc(var(--header-height)/2+6rem)] sm:pb-24 md:pt-[calc(var(--header-height)/2+8rem)] md:pb-32 xl:pt-[calc(var(--header-height)/2+10rem)] xl:pb-40">
-      <h1>About</h1>
-    </Container>
-  );
-}
