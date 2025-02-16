ce43f3090fd9ca4ec0a5a02a276a943a3a49ff45
Lucas Winkler <lucaswinkler@gmail.com>
2025-02-16 03:37:08 -0500

    fix: remove projects from sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,35 +1,35 @@
-import { config } from '@/config';
-import { PROJECTS } from '@/constants/projects';
-import type { MetadataRoute } from 'next';
+import { config } from "@/config";
+import { PROJECTS } from "@/constants/projects";
+import type { MetadataRoute } from "next";
 
 // TODO: Uncomment changeFrequency and priority when the site is live
 export default function sitemap(): MetadataRoute.Sitemap {
-  const projects = PROJECTS.map(project => ({
-    url: `${config.baseUrl}/projects/${project.slug}`,
-    lastModified: new Date(),
-    // changeFrequency: 'yearly' as const,
-    // priority: 0.8,
-  }));
+  // const projects = PROJECTS.map(project => ({
+  //   url: `${config.baseUrl}/projects/${project.slug}`,
+  //   lastModified: new Date(),
+  //   // changeFrequency: 'yearly' as const,
+  //   // priority: 0.8,
+  // }));
 
   return [
     {
       url: config.baseUrl,
       lastModified: new Date(),
-      // changeFrequency: 'monthly',
-      // priority: 1,
+      changeFrequency: "monthly",
+      priority: 1,
     },
-    {
-      url: `${config.baseUrl}/projects`,
-      lastModified: new Date(),
-      // changeFrequency: 'monthly',
-      // priority: 0.9,
-    },
+    // {
+    // url: `${config.baseUrl}/projects`,
+    // lastModified: new Date(),
+    // changeFrequency: 'monthly',
+    // priority: 0.9,
+    // },
     // {
     //   url: `${config.baseUrl}/contact`,
     //   lastModified: new Date(),
     //   changeFrequency: 'yearly',
     //   priority: 0.6,
     // },
-    ...projects,
+    // ...projects,
   ];
 }
