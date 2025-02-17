9b2f2e490a4b6de24a36f309dd1be1821f5383ee
Lucas Winkler <lucaswinkler@gmail.com>
2025-02-16 19:12:09 -0500

    fix: apply smooth-scroll only on md+ to fix scroll issue on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable}`}>
+    <html lang="en" className={`${inter.variable} md:scroll-smooth`}>
       <body className="dark overflow-x-hidden antialiased">
         <Header />
         <main id="main" className="relative">
