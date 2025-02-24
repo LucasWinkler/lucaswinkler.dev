a0bea870c1232087a241be5d2395caa56515e79e
Lucas Winkler <lucaswinkler@gmail.com>
2025-02-23 22:57:54 -0500

    feat(nav): add social links and adjust navigation styles

--- src/components/layout/header/desktop-nav.tsx
+++ src/components/layout/header/desktop-nav.tsx
@@ -8,97 +8,109 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { MAX_SUBLINKS, NAV_LINKS } from "@/constants/links";
-import { cn } from "@/lib/utils";
+import { createSocialLinks, MAX_SUBLINKS, NAV_LINKS } from "@/constants/links";
 
 export const DesktopNav = () => {
-  return (
-    <NavigationMenu
-      role="navigation"
-      className="text-foreground-dark-tertiary hidden md:flex"
-    >
-      <NavigationMenuList>
-        {NAV_LINKS.map(({ href, label, sublinks }) => {
-          const hasSublinks = sublinks && sublinks.links.length > 0;
+  const socialLinks = createSocialLinks();
 
-          return (
-            <NavigationMenuItem key={href}>
-              {hasSublinks ? (
-                <>
-                  <NavigationMenuTrigger
-                    aria-label={`Open ${label} dropdown menu`}
-                    className="font-normal"
-                  >
-                    {label}
-                  </NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                      <li className="row-span-3">
-                        <NavigationMenuLink asChild>
-                          <Link
-                            aria-label={`Go to ${sublinks.label} page`}
-                            className="group/nav-sublink from-muted/50 to-muted hover:from-muted/60 flex h-full w-full flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline transition-colors duration-150 ease-out outline-none select-none focus:shadow-md"
-                            href={href}
-                          >
-                            <span
-                              aria-describedby={`sublink-description-${label.toLowerCase()}`}
-                              className="mt-4 mb-2 text-lg font-medium transition-transform duration-150 ease-out group-hover/nav-sublink:scale-95"
-                            >
-                              {sublinks.label}
-                            </span>
-                            {sublinks.description && (
-                              <p
-                                id={`sublink-description-${label.toLowerCase()}`}
-                                className="text-foreground-dark-secondary text-sm leading-tight transition-transform duration-150 ease-out group-hover/nav-sublink:scale-95"
-                              >
-                                {sublinks.description}
-                              </p>
-                            )}
-                          </Link>
-                        </NavigationMenuLink>
-                      </li>
-                      {sublinks.links.slice(0, MAX_SUBLINKS).map((sublink) => (
-                        <li className="col-span-1" key={sublink.href}>
+  return (
+    <div className="text-foreground-dark-tertiary hidden items-center gap-4 md:flex">
+      <NavigationMenu role="navigation">
+        <NavigationMenuList className="gap-2">
+          {NAV_LINKS.map(({ href, label, sublinks }) => {
+            const hasSublinks = sublinks && sublinks.links.length > 0;
+
+            return (
+              <NavigationMenuItem key={href}>
+                {hasSublinks ? (
+                  <>
+                    <NavigationMenuTrigger
+                      aria-label={`Open ${label} dropdown menu`}
+                      className="font-normal"
+                    >
+                      {label}
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                        <li className="row-span-3">
                           <NavigationMenuLink asChild>
                             <Link
-                              aria-label={`Go to ${sublink.label} page`}
-                              href={sublink.href}
-                              className="hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block space-y-1 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none"
+                              aria-label={`Go to ${sublinks.label} page`}
+                              className="group/nav-sublink from-muted/50 to-muted hover:from-muted/60 flex h-full w-full flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline transition-colors duration-150 ease-out outline-none select-none focus:shadow-md"
+                              href={href}
                             >
                               <span
-                                aria-describedby={`sublink-description-${sublink.label.toLowerCase()}`}
-                                className="text-sm leading-none font-medium"
+                                aria-describedby={`sublink-description-${label.toLowerCase()}`}
+                                className="mt-4 mb-2 text-lg font-medium transition-transform duration-150 ease-out group-hover/nav-sublink:scale-95"
                               >
-                                {sublink.label}
+                                {sublinks.label}
                               </span>
-                              <p
-                                id={`sublink-description-${sublink.label.toLowerCase()}`}
-                                className="text-foreground-dark-secondary line-clamp-2 text-sm leading-snug"
-                              >
-                                {sublink.description}
-                              </p>
+                              {sublinks.description && (
+                                <p
+                                  id={`sublink-description-${label.toLowerCase()}`}
+                                  className="text-foreground-dark-secondary text-sm leading-tight transition-transform duration-150 ease-out group-hover/nav-sublink:scale-95"
+                                >
+                                  {sublinks.description}
+                                </p>
+                              )}
                             </Link>
                           </NavigationMenuLink>
                         </li>
-                      ))}
-                    </ul>
-                  </NavigationMenuContent>
-                </>
-              ) : (
-                <NavigationMenuLink
-                  asChild
-                  className={navigationMenuTriggerStyle({
-                    className:
-                      href === NAV_LINKS[NAV_LINKS.length - 1].href && "pr-0",
-                  })}
-                >
-                  <Link href={href}>{label}</Link>
-                </NavigationMenuLink>
-              )}
-            </NavigationMenuItem>
-          );
-        })}
-      </NavigationMenuList>
-    </NavigationMenu>
+                        {sublinks.links
+                          .slice(0, MAX_SUBLINKS)
+                          .map((sublink) => (
+                            <li className="col-span-1" key={sublink.href}>
+                              <NavigationMenuLink asChild>
+                                <Link
+                                  aria-label={`Go to ${sublink.label} page`}
+                                  href={sublink.href}
+                                  className="hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block space-y-1 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none"
+                                >
+                                  <span
+                                    aria-describedby={`sublink-description-${sublink.label.toLowerCase()}`}
+                                    className="text-sm leading-none font-medium"
+                                  >
+                                    {sublink.label}
+                                  </span>
+                                  <p
+                                    id={`sublink-description-${sublink.label.toLowerCase()}`}
+                                    className="text-foreground-dark-secondary line-clamp-2 text-sm leading-snug"
+                                  >
+                                    {sublink.description}
+                                  </p>
+                                </Link>
+                              </NavigationMenuLink>
+                            </li>
+                          ))}
+                      </ul>
+                    </NavigationMenuContent>
+                  </>
+                ) : (
+                  <NavigationMenuLink
+                    asChild
+                    className={navigationMenuTriggerStyle({
+                      className: "px-3",
+                    })}
+                  >
+                    <Link href={href}>{label}</Link>
+                  </NavigationMenuLink>
+                )}
+              </NavigationMenuItem>
+            );
+          })}
+        </NavigationMenuList>
+      </NavigationMenu>
+      {socialLinks.length > 0 && (
+        <ul className="flex items-center gap-4 border-l border-gray-800 pl-8">
+          {socialLinks.map(({ label, href, icon: Icon }) => (
+            <li key={label}>
+              <Link href={href}>
+                <Icon />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
