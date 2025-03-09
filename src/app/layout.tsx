9ae09bb8c3b5f029d848211b042d9b6132ed8b00
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-08 22:13:35 -0500

    feat: enable color scheme support in next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,7 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             enableSystem
+            enableColorScheme
             disableTransitionOnChange
           >
             <TooltipProvider>
