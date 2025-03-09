79db11a25f0b2cd15fec0a33dcc75dcf858a61fb
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-09 01:23:16 -0500

    style: adjust skip nav colours

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -57,7 +57,7 @@ export const Header = () => {
     >
       <a
         href="#main"
-        className="focus:bg-background focus:ring-ring sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:block focus:rounded-md focus:px-4 focus:py-2 focus:text-white focus:ring-2 focus:outline-none"
+        className="focus:bg-primary focus:ring-ring sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:block focus:rounded-md focus:px-4 focus:py-2 focus:text-primary-foreground focus:ring-2 focus:outline-none"
       >
         Skip to main content
       </a>
