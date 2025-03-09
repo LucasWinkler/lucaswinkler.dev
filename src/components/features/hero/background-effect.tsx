08a3a7475d38562d8e92cf83ce8568cbdc466ad9
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-09 16:16:40 -0400

    style: adjust hero gradient stops to prevent clipping

--- src/components/features/hero/background-effect.tsx
+++ src/components/features/hero/background-effect.tsx
@@ -6,11 +6,11 @@ export const BackgroundEffect = () => {
       <Container className="relative h-full">
         <div className="absolute inset-0">
           {/* Purple gradient - left side */}
-          <div className="absolute top-[40%] -left-[300px] h-[700px] w-[700px] -translate-y-1/2 bg-radial/oklch from-bg-circle-purple-from from-0% via-bg-circle-purple-via via-45% to-transparent to-75% sm:-left-[400px] sm:h-[900px] sm:w-[900px] md:top-[45%] md:-left-[500px] md:h-[1200px] md:w-[1200px]" />
+          <div className="absolute top-[40%] -left-[300px] h-[700px] w-[700px] -translate-y-1/2 bg-radial/oklch from-bg-circle-purple-from from-0% via-bg-circle-purple-via via-40% to-transparent to-65% sm:-left-[400px] sm:h-[900px] sm:w-[900px] md:top-[45%] md:-left-[500px] md:h-[1200px] md:w-[1200px]" />
           {/* Indigo gradient - right side */}
-          <div className="absolute top-[0px] right-[-250px] h-[600px] w-[600px] bg-radial/oklch from-bg-circle-indigo-from from-0% via-bg-circle-indigo-via via-45% to-transparent to-75% sm:top-[-50px] sm:right-[-350px] sm:h-[800px] sm:w-[800px] md:-top-[200px] md:right-[-500px] md:h-[1200px] md:w-[1200px]" />
+          <div className="absolute top-[0px] right-[-250px] h-[600px] w-[600px] bg-radial/oklch from-bg-circle-indigo-from from-0% via-bg-circle-indigo-via via-40% to-transparent to-65% sm:top-[-50px] sm:right-[-350px] sm:h-[800px] sm:w-[800px] md:-top-[200px] md:right-[-500px] md:h-[1200px] md:w-[1200px]" />
           {/* Blue gradient - right side */}
-          <div className="absolute top-[100px] right-[-50px] h-[450px] w-[450px] bg-radial/oklch from-bg-circle-blue-from from-0% via-bg-circle-blue-via via-45% to-transparent to-75% sm:top-[75px] sm:right-[-150px] sm:h-[600px] sm:w-[600px] md:-top-[25px] md:right-[-200px] md:h-[900px] md:w-[900px]" />
+          <div className="absolute top-[100px] right-[-50px] h-[450px] w-[450px] bg-radial/oklch from-bg-circle-blue-from from-0% via-bg-circle-blue-via via-35% to-transparent to-60% sm:top-[75px] sm:right-[-150px] sm:h-[600px] sm:w-[600px] md:-top-[25px] md:right-[-200px] md:h-[900px] md:w-[900px]" />
         </div>
       </Container>
       {/* Dot pattern with fade-out via a mask */}
