8eaed026da0685150bb874572c268fe7578be1a8
Lucas Winkler <lucaswinkler@gmail.com>
2025-03-13 18:17:29 -0400

    feat: enhance project card with alert messages
    - Introduced alert messages in the project card for better user feedback
    - Added a new project "Elevate Careers" with a warning alert regarding demo functionality
    - Updated project alt texts

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -10,7 +10,7 @@ export const PROJECTS: Project[] = [
       "A web app that lets users discover and save points of interest. Features include custom list creation, nearby search, user profiles, and social sharing capabilities.",
     image: {
       src: poitogo,
-      alt: "PoiToGo",
+      alt: "PoiToGo Homepage",
     },
     tags: [
       "Next.js",
@@ -25,28 +25,13 @@ export const PROJECTS: Project[] = [
     },
   },
   {
-    title: "Online Learning Platform",
-    slug: "online-learning-platform",
-    description:
-      "A learning management system that allows the teacher to create and sell courses while students can enroll and attend the lessons with a dashboard and progress tracking.",
-    image: {
-      src: undefined,
-      alt: "Online Learning Platform",
-    },
-    tags: ["Next.js", "TypeScript", "Tailwind CSS", "PostgreSQL", "Prisma"],
-    links: {
-      demo: "https://lw-online-learning-platform.vercel.app/",
-      source: "https://github.com/LucasWinkler/online-learning-platform",
-    },
-  },
-  {
     title: "Audiophile Ecommerce",
     slug: "audiophile-ecommerce",
     description:
       "An e-commerce solution for a fictional audio equipment store. It features a fully responsive design and a mock checkout process. The goal was to follow the original design with precision.",
     image: {
       src: audiophile,
-      alt: "Audiophile Ecommerce",
+      alt: "Audiophile Ecommerce Homepage",
     },
     tags: ["Next.js", "TypeScript", "Tailwind CSS"],
     links: {
@@ -54,4 +39,39 @@ export const PROJECTS: Project[] = [
       source: "https://github.com/LucasWinkler/audiophile-ecommerce",
     },
   },
+  {
+    title: "Elevate Careers",
+    slug: "elevate-careers",
+    description:
+      "A job board that scrapes job listings from Indeed and displays them in a clean and organized way. The goal was to practice webscraping and build a useful tool for job seekers.",
+    alert: {
+      type: "warning",
+      message:
+        "Demo is currently non-functional due to webscraping API costs. Please check out the source code instead!",
+    },
+    image: {
+      src: undefined,
+      alt: "Elevate Careers Homepage",
+    },
+    tags: ["Next.js", "TypeScript", "Tailwind CSS", "Webscraping"],
+    links: {
+      demo: "https://elevate-careers.vercel.app/",
+      source: "https://github.com/LucasWinkler/elevate-careers",
+    },
+  },
+  // {
+  //   title: "Online Learning Platform",
+  //   slug: "online-learning-platform",
+  //   description:
+  //     "A learning management system that allows the teacher to create and sell courses while students can enroll and attend the lessons with a dashboard and progress tracking.",
+  //   image: {
+  //     src: undefined,
+  //     alt: "Online Learning Platform",
+  //   },
+  //   tags: ["Next.js", "TypeScript", "Tailwind CSS", "PostgreSQL", "Prisma"],
+  //   links: {
+  //     demo: "https://lw-online-learning-platform.vercel.app/",
+  //     source: "https://github.com/LucasWinkler/online-learning-platform",
+  //   },
+  // },
 ] as const;
