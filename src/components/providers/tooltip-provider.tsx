408be7428e3e3b6e7c8814c4ae5e4468904fe265
Lucas Winkler <lucaswinkler@teeon.com>
2026-06-27 19:19:49 -0400

    Remove all Next.js/version 1 files

--- src/components/providers/tooltip-provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { TooltipProvider as RadixTooltipProvider } from "@/components/ui/tooltip";
-
-export function TooltipProvider({ children }: { children: React.ReactNode }) {
-  return <RadixTooltipProvider>{children}</RadixTooltipProvider>;
-}
