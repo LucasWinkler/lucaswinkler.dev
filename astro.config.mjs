221664a55dc8c5513d9252bcb57ee6babd7f393f
Lucas Winkler <lucaswinkler@teeon.com>
2026-06-28 22:04:23 -0400

    Add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: 'https://www.lucaswinkler.dev',
   trailingSlash: 'never',
   integrations: [react()],
